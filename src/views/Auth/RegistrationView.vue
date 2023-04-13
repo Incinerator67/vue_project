c0545bf6284e52adaa28bf5dcd93ab9059ea8716
Incinerator67 <edemosmanov2699@gmail.com>
2023-04-13 03:01:50 +0300

    deal with Orders

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -35,7 +35,7 @@
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary">Registration</v-btn>
     </v-card-actions>
   </v-card>
   </v-col>
