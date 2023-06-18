cffd7d9a8f2f6d6f4354a78a2158a02769120bc4
Incinerator67 <edemosmanov2699@gmail.com>
2023-06-18 19:29:07 +0300

    deal with orders

--- src/store/ads.js
+++ src/store/ads.js
@@ -51,6 +51,26 @@ export default {
       payload.userId = getters.user != null ? getters.user.id : '1'
     }
   },
+  async updateAd ({commit},{title,desc,id}) {
+    commit('clearError')
+    commit('setLoading', true)
+    //Заглушка запроса
+    let isRequestOk = true
+    let promise = new Promise(function(resolve) {resolve('Done')});
+    if (isRequestOk) {
+      await promise.then(()=> {
+        commit('updateAd',{ title, desc, id})
+        commit('setLoading', false)
+      })
+    } 
+    else {
+      await promise.then(()=> {
+        commit('setLoading', false)
+        commit('setError', 'Ошибка редактирования объявления')
+        throw 'Упс... Ошибка редактирования объявления'
+      })
+    }  
+  },
   getters: {
     ads(state) {return state.ads},
     promoAds(state) {
@@ -89,26 +109,6 @@ export default {
         })
       }
     },
-    async updateAd ({commit},{title,desc,id}) {
-      commit('clearError')
-      commit('setLoading', true)
-      //Заглушка запроса
-      let isRequestOk = true
-      let promise = new Promise(function(resolve) {resolve('Done')});
-      if (isRequestOk) {
-        await promise.then(()=> {
-          commit('updateAd',{ title, desc, id})
-          commit('setLoading', false)
-        })
-      } 
-      else {
-        await promise.then(()=> {
-          commit('setLoading', false)
-          commit('setError', 'Ошибка редактирования объявления')
-          throw 'Упс... Ошибка редактирования объявления'
-        })
-      }
-    }
   }
 }
     
--- src/store/order.js
+++ src/store/order.js
@@ -1,7 +1,21 @@
+class Order {
+	constructor(name, phone, adId, userId, done = false, id = null) {
+	this.name = name
+	this.phone = phone
+	this.adId = adId
+	this.userId = userId
+	this.done = done
+	this.id = id
+	}
+	}
 export default {
 	state: {},
-	mutations: {},
-	actions: {async createOrder({commit},{name, phone, adId, userId}) {
+	mutations: {
+		createOrder(state, payload){state.orders.push(payload)},
+	},
+	actions: {
+		async createOrder({commit},{name, phone, adId, userId}) {
+		let payload = new Order(name, phone, adId, userId, false, Math.random())
 		console.log(name)
 		console.log(phone)
 		console.log(adId)
@@ -10,20 +24,20 @@ export default {
 		//Заглушка запроса
 		let isRequestOk = true
 		let promise = new Promise(function(resolve) {
-		setTimeout(() => resolve('Done')
-		, 3000);
-		});
-		if (isRequestOk) {
-		await promise.then(()=> {
-		//Здесь вызовем commit для добавления заказа
-		})
-		} else {
-		await promise.then(()=> {
-		commit('setError', 'Ошибка создания заказа')
-		throw 'Упс... Ошибка создания заказа'
-		})
-		}
-		}
-		},
-	getters: {}
+            setTimeout(() => resolve('Done'), 3000);});
+            if (isRequestOk) {
+                await promise.then(()=> {
+                    commit('createOrder', payload)
+                    //Здесь вызовем commit для добавления заказа
+                })
+            } 
+            else {
+                await promise.then(()=> {
+                    commit('setError', 'Ошибка создания заказа')
+                    throw 'Упс... Ошибка создания заказа'
+                })
+            }
+	    }
+    },
+    getters: {}
 }
